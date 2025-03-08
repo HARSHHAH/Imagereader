a005874bbff016a04563ab7c715e207af486bf4d
Harsh Shah <harshshahth14@gmail.com>
2025-03-07 23:05:59 -0500

    Added comments explaining code

--- Backend/app.py
+++ Backend/app.py
@@ -1,24 +1,37 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import pytesseract
-from PIL import Image
-import io
+from flask import Flask, request, jsonify 
+from flask_cors import CORS  # Allows cross-origin requests so frontend can talk to backend
+import pytesseract #Our character recognition library
+from PIL import Image #Let's us handle images
+import io  # Helps convert image bytes to an actual image object
 
+# Tell pytesseract where Tesseract-OCR is installed, necessary for windows at least
+
+pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 app = Flask(__name__)
+
+#Lets frontend make requests to backend
 CORS(app)  
 
 @app.route("/upload", methods=["POST"])
+
 def upload_image():
+
+    # Check if an image was actually uploaded
     if "image" not in request.files:
         return jsonify({"error": "No image uploaded"}), 400
     
+    #Gets the actual image from the request
     image = request.files["image"]
+    # Converts the image from binary data to an actual image object
     img = Image.open(io.BytesIO(image.read()))
 
+    #Extract text from image via tesseract
     extracted_text = pytesseract.image_to_string(img)
 
+    #Extracted into a JSON response
     return jsonify({"text": extracted_text})
 
 if __name__ == "__main__":
+    #Debug=true gives us helpful error messages when debugging
     app.run(debug=True)
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,23 +2,28 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
+  // State to store the uploaded image file and extracted text
   const [image, setImage] = useState(null);
   const [text, setText] = useState("");
 
+  //Function that handles when a user selects an image
   const handleImageUpload = (event) => {
     setImage(event.target.files[0]);
   };
 
+  // Handles sending the image to the backend for text extraction
   const handleSubmit = async () => {
+    //This prevents the user from submitting without selecting an actual image
     if (!image) return alert("Please select an image!");
 
     const formData = new FormData();
     formData.append("image", image);
-
+    // Send the image to the backend using an HTTP POST request
     try {
       const response = await axios.post("http://127.0.0.1:5000/upload", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
+      //Update text state from backend response and handle any errors
       setText(response.data.text);
     } catch (error) {
       console.error("Error uploading image:", error);
